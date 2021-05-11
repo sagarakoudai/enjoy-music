43aa9ec75c64c7b7ba6a9000e9a3271885fa3a0e
sagarakoudai <magnons180@gmail.com>
2021-05-11 20:18:54 +0000

    [add] song_show,edit models_relation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,11 @@
 class Song < ApplicationRecord
   mount_uploader :file, AudioFileUploader
+  attachment :image
 
   belongs_to :user
-  belongs_to :song_style, optional: true
+  belongs_to :song_style
+  has_many :favorites
+  has_many :comments
 
   validates :title, presence: true
   validates :file, presence: true
