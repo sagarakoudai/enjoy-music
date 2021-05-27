c21cfec8aed9a8668ac2f1a77eeb4976992f4c5e
sagarakoudai <magnons180@gmail.com>
2021-05-27 13:13:54 +0000

    mp3変換を消した

--- app/uploaders/audio_file_uploader.rb
+++ app/uploaders/audio_file_uploader.rb
@@ -47,12 +47,12 @@ class AudioFileUploader < CarrierWave::Uploader::Base
   # end
 
 # mp3に変換
-  version :mp3 do
-    process :convert => [{output_format: :mp3}]
+  # version :mp3 do
+  #   process :convert => [{output_format: :mp3}]
 
-    def full_filename(for_file)
-      "#{super.chomp(File.extname(super))}.mp3"
-    end
-  end
+  #   def full_filename(for_file)
+  #     "#{super.chomp(File.extname(super))}.mp3"
+  #   end
+  # end
 
 end
