d8c2ab3f439dd7055a1c3beea014dac57e6daeeb
sagarakoudai <magnons180@gmail.com>
2021-06-11 21:22:44 +0000

    [add]songのsystemテスト

--- spec/system/songs_spec.rb
+++ spec/system/songs_spec.rb
@@ -16,18 +16,18 @@ RSpec.describe "Songs", type: :system do
 	  expect(page).to have_content "新しい曲"
 	end
 
-	# it "楽曲を投稿する" do
- #   visit root_path
-	# 	click_link "Sign in"
-	# 	fill_in 'user[email]', with: @user.email
-	# 	fill_in 'user[password]', with: @user.password
-	# 	click_button "Log in"
+	it "楽曲を投稿する、その後indexページへ" do
+    visit root_path
+		click_link "Sign in"
+		fill_in 'user[email]', with: @user.email
+		fill_in 'user[password]', with: @user.password
+		click_button "Log in"
 
-	#   click_link "New Song"
-	# 	attach_file "song[file]", "#{Rails.root}/spec/files/My_Song_6.mp3"
-	#   fill_in "song[title]", with: "testsong"
-	# 	click_button "アップロード"
-	# 	expect(page).to have_content "was successfully created"
-	# end
+	  click_link "New Song"
+		attach_file "song_file", "#{Rails.root}/spec/files/My_Song_6.mp3"
+	  fill_in "song[title]", with: "testsong"
+		click_button "アップロード"
+		expect(page).to have_content "testsong"
+	end
 
 end
