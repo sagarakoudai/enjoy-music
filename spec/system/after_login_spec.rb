496d72a90fb5c56b22838f42acfdae8919abcc13
sagarakoudai <magnons180@gmail.com>
2021-06-21 18:46:50 +0000

    [add] ログイン後のテスト

--- spec/system/after_login_spec.rb
+++ spec/system/after_login_spec.rb
@@ -15,11 +15,31 @@ RSpec.describe "ログイン後のテスト", type: :system do
 
 
   describe "ヘッダーのテスト:ログイン後" do
-    it "タイトルを押すとトップ画面に遷移する"
-    it "New Songを押すと楽曲投稿画面に遷移する"
-    it "My Pageを押すとマイページ画面に遷移する"
-    it "Songsを押すと楽曲一覧画面に遷移する"
-    it "Sign outを押すとログアウトし、トップ画面に遷移する"
+    it "タイトルを押すとトップ画面に遷移する" do
+      click_link "ENJOY MUSIC!", match: :first
+      expect(page).to have_content "ENJOY MUSIC!へようこそ！"
+    end
+
+    it "New Songを押すと楽曲投稿画面に遷移する" do
+      click_link "New Song"
+      expect(page).to have_content "楽曲ファイル"
+    end
+
+    it "My Pageを押すとマイページ画面に遷移する" do
+      click_link "My Page"
+      expect(page).to have_content "testuser"
+    end
+
+    it "Songsを押すと楽曲一覧画面に遷移する" do
+      click_link "Songs"
+      expect(page).to have_content "example title"
+    end
+
+    it "Sign outを押すとログアウトし、トップ画面に遷移する" do
+      click_link "Sign Out"
+      expect(page).to have_content "新規登録"
+      # トップ画面にログイン前のみ表示される新規登録リンクが表示されているか
+    end
   end
 
   describe "楽曲投稿機能" do
@@ -31,12 +51,26 @@ RSpec.describe "ログイン後のテスト", type: :system do
       expect(page).to have_content "testsong"
     end
   end
-  
+
   describe "楽曲編集機能" do
-    it "楽曲の編集 完了後、楽曲一覧ページへ"
+    it "楽曲の編集 完了後、楽曲一覧ページへ" do
+      click_link "Songs"
+      click_link "example title"
+      click_link "編集"
+      attach_file "song_file", "#{Rails.root}/spec/files/My_Song_6.mp3"
+      fill_in "song[title]", with: "testsong_edit"
+      click_button "アップロード"
+      expect(page).to have_content "testsong_edit"
+    end
   end
-  
+
   describe "プロフィール編集" do
-    it "プロフィールの編集 完了後、マイページへ"
+    it "プロフィールの編集 完了後、マイページへ" do
+      click_link "My Page"
+      click_link "プロフィールを編集する"
+      fill_in "user[name]", with: "testuser_edit"
+      click_button "プロフィール変更"
+      expect(page).to have_content "testuser_edit"
+    end
   end
 end
